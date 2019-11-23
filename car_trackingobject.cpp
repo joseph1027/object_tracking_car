aed54491d2f82c5af69cb26a73922c293afb0e63
Joseph Leslie Hsueh <joseph102712080104@gmail.com>
2019-11-23 19:57:39 +0800

    Update car_trackingobject.cpp

--- car_trackingobject.cpp
+++ car_trackingobject.cpp
@@ -41,7 +41,6 @@ string intToString(int number) {
 	return ss.str();
 }
 
-// User draws box around object to track. This triggers CAMShift to start tracking
 static void onMouse(int event, int x, int y, int, void*)
 {
 	if (selectObject)
@@ -64,7 +63,7 @@ static void onMouse(int event, int x, int y, int, void*)
 	case EVENT_LBUTTONUP://左鍵上
 		selectObject = false;
 		if (selection.width > 0 && selection.height > 0)
-			trackObject = -1;   // Set up CAMShift properties in main() loop
+			trackObject = -1;
 		break;
 	}
 }
@@ -78,42 +77,6 @@ string hot_keys =
 "\tp - pause video\n"
 "To initialize tracking, select the object with mouse\n";
 
-/*static void help()
-{
-cout << "\nThis is a demo that shows mean-shift based tracking\n"
-"You select a color objects such as your face and it tracks it.\n"
-"This reads from video camera (0 by default, or the camera number the user enters\n"
-"Usage: \n"
-"   ./camshiftdemo [camera number]\n";
-cout << hot_keys;
-}*/
-
-/*const char* keys =
-{
-"{help h | | show help message}{@camera_number| 0 | camera number}"
-};*/
-
-/*int foo()
-{
-	int sum = 0;
-	for (int i = 0; i <= 1000; i++)
-	{
-		sum = sum + i;
-	}
-	cout <<"sum:"<< sum << endl;
-	return 0;
-}
-
-int goo(int input)
-{
-	int summ = 1;
-	for (int k = 1; k < input; k++)
-	{
-		summ = summ * k;
-	}
-	cout <<"summ:"<< summ << endl;
-	return 0;
-}*/
 
 
 int main(int argc, const char** argv)
@@ -152,15 +115,6 @@ int main(int argc, const char** argv)
 
 	cap.open(0);//"http://arduino.local:8080/?action=stream"
 	//VideoCapture cap("234.mp4");//234.mp4
-
-	/*if (!cap.isOpened())
-	{
-	help();
-	cout << "***Could not initialize capturing...***\n";
-	cout << "Current parameter's value: \n";
-	parser.printMessage();
-	return -1;
-	}*/
 	cout << hot_keys;
 	namedWindow("Histogram", 0);
 	namedWindow("CamShift Demo", 0);
@@ -177,21 +131,18 @@ int main(int argc, const char** argv)
 	{
 		if (!paused)
 		{
-			cap >> frame;//get a new frame from camera
-						 /*if (frame.empty())
-						 break;*/
-			
+			cap >> frame;
 		}
 
 		frame.copyTo(image);
 
 		if (!paused)
 		{
-			cvtColor(image, hsv, COLOR_BGR2HSV);//bgr to hsv
+			cvtColor(image, hsv, COLOR_BGR2HSV);
 
-			if (trackObject)//tracking enalbe
+			if (trackObject)
 			{
-				int _vmin = vmin, _vmax = vmax;//track bar
+				int _vmin = vmin, _vmax = vmax;
 				int _smin = smin, _smax = smax;
 				inRange(hsv, Scalar(0, MIN(_smin, _smax), MIN(_vmin, _vmax)),
 					Scalar(180, MAX(_smin, _smax), MAX(_vmin, _vmax)), mask);
@@ -201,13 +152,12 @@ int main(int argc, const char** argv)
 
 				if (trackObject < 0)
 				{
-					// Object has been selected by user, set up CAMShift search properties once
 					Mat roi(hue, selection), maskroi(mask, selection);
 					calcHist(&roi, 1, 0, maskroi, hist, 1, &hsize, &phranges);
 					normalize(hist, hist, 0, 255, NORM_MINMAX);
 
 					trackWindow = selection;
-					trackObject = 1; // Don't set up again, unless user selects new ROI
+					trackObject = 1;
 
 					histimg = Scalar::all(0);
 					int binW = histimg.cols / hsize;
@@ -224,8 +174,6 @@ int main(int argc, const char** argv)
 							Scalar(buf.at<Vec3b>(i)), -1, 8);
 					}
 				}
-
-				// Perform CAMShift
 				calcBackProject(&hue, 1, 0, hist, backproj, &phranges);
 				backproj &= mask;
 				RotatedRect trackBox = CamShift(backproj, trackWindow,
@@ -240,13 +188,13 @@ int main(int argc, const char** argv)
 
 				if (backprojMode)
 					cvtColor(backproj, image, COLOR_GRAY2BGR);
-				ellipse(image, trackBox, Scalar(0, 255, 0), 2, LINE_AA);//LINE_AA
+				ellipse(image, trackBox, Scalar(0, 255, 0), 2, LINE_AA);
 				cout << trackBox.center << " ";
 				cout << "{" << trackBox.size.height << " , ";
 				cout << trackBox.size.width << "}" << endl;
 				circle(image, trackBox.center, 2, Scalar(0, 0, 255), 2);
 				putText(image, intToString(trackBox.center.x) + "," + intToString(trackBox.center.y), trackBox.center, 1, 1, Scalar(0, 0, 255), 2);
-				if ((trackBox.center.x < 320 && prevv == 0) || prevv == 2) {// && prevv==0)||prevv==2
+				if ((trackBox.center.x < 320 && prevv == 0) || prevv == 2) {
 					curl_easy_setopt(curl, CURLOPT_URL, "http://arduino2.local/arduino/digital/12/0");
 					res = curl_easy_perform(curl);
 					curl_easy_setopt(curl, CURLOPT_URL, "http://arduino2.local/arduino/digital/10/1");
@@ -279,8 +227,8 @@ int main(int argc, const char** argv)
 		imshow("CamShift Demo", image);
 		imshow("Histogram", histimg);
 
-		char c = (char)waitKey(10);//等待10ms 若有按鍵按下 return ASCII 否則-1,若參數0 則無限等下去直到按鍵按下
-		if (c == 27) //ESC 按鍵
+		char c = (char)waitKey(10);
+		if (c == 27)
 			break;
 		switch (c)
 		{
@@ -289,7 +237,7 @@ int main(int argc, const char** argv)
 			break;
 		case 'c':
 			trackObject = 0;
-			histimg = Scalar::all(0);//將scalar全部元素初始化為0
+			histimg = Scalar::all(0);
 			break;
 		case 'h':
 			showHist = !showHist;
