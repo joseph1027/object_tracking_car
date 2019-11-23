1362c9424218735b947277b8f46f6025f1629a70
Joseph Leslie Hsueh <joseph102712080104@gmail.com>
2019-11-23 19:58:11 +0800

    Update car_trackingobject.cpp

--- car_trackingobject.cpp
+++ car_trackingobject.cpp
@@ -55,12 +55,12 @@ static void onMouse(int event, int x, int y, int, void*)
 
 	switch (event)
 	{
-	case EVENT_LBUTTONDOWN://左鍵下
+	case EVENT_LBUTTONDOWN:
 		origin = Point(x, y);
 		selection = Rect(x, y, 0, 0);
 		selectObject = true;
 		break;
-	case EVENT_LBUTTONUP://左鍵上
+	case EVENT_LBUTTONUP:
 		selectObject = false;
 		if (selection.width > 0 && selection.height > 0)
 			trackObject = -1;
